7307db2b0bc17c5bb4cecccef326b0c20786e9f5
thanhdat369 <dat.lethanh369@gmail.com>
2023-10-02 22:30:15 +0700

    Add demo translation I18N

---
+++ TOMATODO/translations/english.ts
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DeadlineBox</name>
+    <message>
+        <location filename="../qml/components/DeadlineBox.qml" line="54"/>
+        <source>min</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PomodoroInfoTaskCard</name>
+    <message>
+        <location filename="../qml/components/PomodoroInfoTaskCard.qml" line="36"/>
+        <source>30 min</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PomodoroPicker</name>
+    <message>
+        <location filename="../qml/components/PomodoroPicker.qml" line="145"/>
+        <source>Edit Task</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/components/PomodoroPicker.qml" line="147"/>
+        <source>Create Task</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/components/PomodoroPicker.qml" line="150"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsScreen</name>
+    <message>
+        <location filename="../qml/screen/SettingsScreen.qml" line="53"/>
+        <source>Languague</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="23"/>
+        <source>TOMATODO App</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ TOMATODO/translations/vietnamese.ts
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi_VN">
+<context>
+    <name>DeadlineBox</name>
+    <message>
+        <location filename="../qml/components/DeadlineBox.qml" line="54"/>
+        <source>min</source>
+        <translation>phút</translation>
+    </message>
+</context>
+<context>
+    <name>PomodoroInfoTaskCard</name>
+    <message>
+        <location filename="../qml/components/PomodoroInfoTaskCard.qml" line="36"/>
+        <source>30 min</source>
+        <translation>30 phut</translation>
+    </message>
+</context>
+<context>
+    <name>PomodoroPicker</name>
+    <message>
+        <location filename="../qml/components/PomodoroPicker.qml" line="145"/>
+        <source>Edit Task</source>
+        <translation>Sua Task</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/PomodoroPicker.qml" line="147"/>
+        <source>Create Task</source>
+        <translation>Tao Task</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/PomodoroPicker.qml" line="150"/>
+        <source>Title</source>
+        <translation>Tieu de</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsScreen</name>
+    <message>
+        <location filename="../qml/screen/SettingsScreen.qml" line="53"/>
+        <source>Languague</source>
+        <translation> Ngon ngu</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="23"/>
+        <source>TOMATODO App</source>
+        <translation>App Ca Chua</translation>
+    </message>
+</context>
+</TS>
